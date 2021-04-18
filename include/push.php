ca5a250602d1410a0c0adf7c1b58dafdfb8d8a49
蚪侠站群 <60836406+douxiaseo@users.noreply.github.com>
2021-04-18 20:04:03 +0800

    后台完善，界面简单直观易懂，功能齐全，全网第一镜像系统。适配多级反向代理
    后台完善，界面简单直观易懂，功能齐全，全网第一镜像系统。适配多级反向代理

---
+++ include/push.php
@@ -0,0 +1,53 @@
+<?php
+/***主动推送***/
+/**********************************函数解释*********************************************************
+
+当前域名：$_SERVER['HTTP_HOST']
+当前主域名：jy9flvuntuxkwfwr()
+随机数字：bpwoswn6df5xiqxg(6)   6为长度，长度可自定义
+随机字符：zas1m3a80jiq3_ii(5)   5为长度，长度可自定义
+
+****************************************************************************************************/
+
+$urls = array(
+
+/*以下是推送的URL地址，默认推送4条，可自定义增加或删减*/
+
+    'http://'.$_SERVER['HTTP_HOST'].'/'.zas1m3a80jiq3_ii(5),
+    'http://'.$_SERVER['HTTP_HOST'].'/'.zas1m3a80jiq3_ii(5).'/'.bpwoswn6df5xiqxg(6).'.html',
+    'http://'.$_SERVER['HTTP_HOST'].'/'.zas1m3a80jiq3_ii(5).'/'.zas1m3a80jiq3_ii(5).'.html',
+    'http://'.$_SERVER['HTTP_HOST'].'/'.zas1m3a80jiq3_ii(5).'/'.zas1m3a80jiq3_ii(5)
+	
+/*以上是推送的URL地址，默认推送4条，可自定义增加或删减*/
+	
+);
+
+
+/***********************************以下是推送代码，有能力的话可以自己二次开发推送代码！******************************************/
+$apiarr = array(
+
+/***将以下推送接口替换成自己的【神马】与【百度】的推送接口即可！***/
+
+    'http://data.zhanzhang.sm.cn/push?site=www.xxxx.win&user_name=1662383315@qq.com&resource_name=mip_add&token=TI_a11b85869ad3c34f9cbe54a19f4163',
+    'http://data.zz.baidu.com/urls?site=www.xxxxx.win&token=tSreGo1HoiuOIg5Ub&type=mip'
+	
+/***将以上推送接口替换成自己的【神马】与【百度】的推送接口即可！***/	
+	
+);
+
+/***以下数字2为推送接口条数，根据自己的实际接口数进行修改***/
+for($i=0;$i<2;$i++){
+if(stripos($apiarr[$i],$_SERVER['HTTP_HOST'])){
+$api = $apiarr[$i];
+$ch = curl_init();
+$options =  array(
+    CURLOPT_URL => $api,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => implode("\n", $urls),
+    CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
+);
+curl_setopt_array($ch, $options);
+$result = curl_exec($ch);
+ }
+}
